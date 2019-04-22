a3b669f11707748c90b79c1b9dbed30c157adf12
Humberto Diaz <hu.diazc@gmail.com>
2019-04-21 22:23:43 -0400

    Version Beta

---
+++ Assets/Dropdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dropdown : MonoBehaviour
+{
+    public RectTransform contenedor;
+    public bool isOpen;
+    internal int value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contenedor = transform.Find("contenedor").GetComponent<RectTransform>();
+        isOpen = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen)
+        {
+            Vector3 scale = contenedor.localScale;
+            scale.y = Mathf.Lerp(scale.y, isOpen ? 1 : 0, Time.deltaTime * 12);
+            contenedor.localScale = scale;
+
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/SelecPiso.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelecPiso : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Piso1(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Piso2(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Piso3(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Piso4(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Piso5(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Piso6(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Piso7(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
